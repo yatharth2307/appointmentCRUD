f74cc8071042ef72df74c414ca4613261425dd28
Anubhav357 <68887591+Anubhav357@users.noreply.github.com>
2022-05-25 16:30:21 +0530

    Update AppointmentCrudApplication.java

--- src/main/java/com/example/appointmentCRUD/AppointmentCrudApplication.java
+++ src/main/java/com/example/appointmentCRUD/AppointmentCrudApplication.java
@@ -1,8 +1,5 @@
 package com.example.appointmentCRUD;
 
-import org.apache.log4j.Logger;  
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,10 +7,9 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class AppointmentCrudApplication {
+
 	public static void main(String[] args) {
-		final Logger log = Logger.getLogger(AppointmentCrudApplication.class.getName());  
-		log.debug("Hello this is a debug message");  
-	    log.info("Hello this is an info message");  
 		SpringApplication.run(AppointmentCrudApplication.class, args);
+		
 	}
 }
